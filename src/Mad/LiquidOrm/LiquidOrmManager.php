cf637ca0e085795deea0893cf95265d9eada7db0
Mustafa Madkour <mustafamdkr@gmail.com>
2022-10-05 12:06:42 +0200

    Documenting methods and vars

--- src/Mad/LiquidOrm/LiquidOrmManager.php
+++ src/Mad/LiquidOrm/LiquidOrmManager.php
@@ -15,14 +15,34 @@ use Mad\LiquidOrm\QueryBuilder\QueryBuilderFactory;
 class LiquidOrmManager
 {
 
+    /**
+     * @var string
+     */
     protected string $tableSchema;
 
+    /**
+     * @var string
+     */
     protected string $tableSchemaID;
 
+    /**
+     * @var array
+     */
     protected array $options;
 
+    /**
+     * @var DataMapperEnvConfig
+     */
     protected DataMapperEnvConfig $envireonmentConfiguration;
 
+    /**
+     * Main class constructor
+     *
+     * @param DataMapperEnvConfig $envireonmentConfiguration
+     * @param string              $tableSchema
+     * @param string              $tableSchemaID
+     * @param array|null          $options
+     */
     public function __construct(DataMapperEnvConfig $envireonmentConfiguration, string $tableSchema, string $tableSchemaID, ?array $options = []) {
         $this->envireonmentConfiguration = $envireonmentConfiguration;
         $this->tableSchema = $tableSchema;
@@ -30,6 +50,12 @@ class LiquidOrmManager
         $this->options = $options;
     }
 
+    /**
+     * An intialization method which glues all the components together and inject
+     * the necessary dependency within the respective object
+     *
+     * @return Object
+     */
     public function intialize()
     {
         $dataMapperFactory = new DataMapperFactory();
