027dbdf801550f25c74993d2f0816868b5a8b3b9
Mustafa Madkour <mustafamdkr@gmail.com>
2022-10-19 13:28:58 +0200

    documenting UserModel

--- App/Model/UserModel.php
+++ App/Model/UserModel.php
@@ -9,16 +9,32 @@ use Mad\Abstracts\AbstractBaseModel;
 class UserModel extends AbstractBaseModel
 {
 
+    /** @var string */
     protected const TABLESCHEMA = 'users';
 
+    /** @var string */
     protected const TABLESCHEMAID = 'id';
 
+
+    /**
+     * Main constructor class which passes the relevant arguments to the 
+     * base model parent constructor. This allows the repository to fetch the
+     * correct information from the database based on the model/entity.
+     * 
+     * @throws BaseInvalidArgException
+     * @return void
+     */
     public function __construct()
     {
         parent::__construct(self::TABLESCHEMA, self::TABLESCHEMAID);
     }
 
 
+    /**
+     * @inheritDoc
+     *
+     * @return array
+     */
     public function guardedID(): array
     {
         return [];
